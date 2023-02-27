9c3fc811613cd4c572e1ef1e8d6d2cbfe4fbed5d
Muhammed Ramzy <57300921+Mohamedramzii@users.noreply.github.com>
2023-02-27 20:35:03 +0200

    singleton service locator getit is done

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,11 @@
+import 'package:book_app/core/api_services.dart';
+import 'package:book_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  getIt.registerSingleton<ApiServices>(ApiServices(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(HomeRepoImpl(getIt.get<ApiServices>()));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 // ignore_for_file: unused_element
 
 import 'package:book_app/constants.dart';
+import 'package:book_app/core/api_services.dart';
 import 'package:book_app/core/utils/app_router.dart';
+import 'package:book_app/core/utils/service_locator.dart';
+import 'package:book_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:book_app/features/home/presentation/view_model/cubits/featured_books_Cubit/featured_books_cubit.dart';
+import 'package:book_app/features/home/presentation/view_model/cubits/newest_books_Cubit/newest_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -19,14 +26,28 @@ class MyApp extends StatelessWidget {
       designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
-      builder: (context, _) => MaterialApp.router(
-        routerConfig: App_Router.router,
-        debugShowCheckedModeBanner: false,
-        title: 'Bookipidea',
-        theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: KprimaryColor,
-          textTheme:
-              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      builder: (context, _) => MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => FeaturedBooksCubit(
+              getIt.get<HomeRepoImpl>()
+            ),
+          ),
+          BlocProvider(
+            create: (context) => NewestBooksCubit(
+             getIt.get<HomeRepoImpl>()
+            ),
+          ),
+        ],
+        child: MaterialApp.router(
+          routerConfig: App_Router.router,
+          debugShowCheckedModeBanner: false,
+          title: 'Bookipidea',
+          theme: ThemeData.dark().copyWith(
+            scaffoldBackgroundColor: KprimaryColor,
+            textTheme:
+                GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+          ),
         ),
       ),
     );
