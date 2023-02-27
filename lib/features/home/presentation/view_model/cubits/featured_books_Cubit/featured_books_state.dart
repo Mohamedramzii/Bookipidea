f2698cce81b7c8bc7222c2117acfa1fb136772ab
Muhammed Ramzy <57300921+Mohamedramzii@users.noreply.github.com>
2023-02-27 18:55:21 +0200

    create featured books method in cubit

---
+++ lib/features/home/presentation/view_model/cubits/featured_books_Cubit/featured_books_state.dart
@@ -0,0 +1,26 @@
+part of 'featured_books_cubit.dart';
+
+abstract class FeaturedBooksState extends Equatable {
+  const FeaturedBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
+
+class FeaturedBooksLoadingState extends FeaturedBooksState {}
+  
+class FeaturedBooksSuccessState extends FeaturedBooksState {
+  final List<BookModel> books;
+  const FeaturedBooksSuccessState({
+    required this.books,
+  });
+}
+
+class FeaturedBooksFailureState extends FeaturedBooksState {
+  final String errMessage;
+  const FeaturedBooksFailureState({
+    required this.errMessage,
+  });
+}
